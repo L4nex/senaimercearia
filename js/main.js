78ab12b6924c4144690de5c98ccc70347b4c86a1
Lucas <mchadolucas@gmail.com>
2020-07-05 18:52:35 -0300

    datatables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+
+$(document).ready(function () {
+
+  $('table').DataTable({
+      language: {
+          url: 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json'
+      }
+  });
+});
+
 feather.replace()
 
 var ctx = document.getElementById("myChart");
